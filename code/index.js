ba7ff7e9fa49c9062db86918191806fec3959b4a
UpendraPolisetty <polisettyupendra@gmail.com>
2023-03-28 10:37:50 +0530

    color generate example

---
+++ code/index.js
@@ -0,0 +1,7 @@
+let box1 = document.querySelector('.box1');
+let box2 = document.querySelector('.box2');
+let btn1 = document.querySelector('.btn1');
+let btn2 = document.querySelector('.btn2');
+
+
+btn1.addEventListener('click' , )
