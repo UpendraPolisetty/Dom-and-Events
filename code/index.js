9323aed96d6eb286b86958e9bca47d358f696028
UpendraPolisetty <polisettyupendra@gmail.com>
2023-04-04 19:38:10 +0530

    Random color generate app

--- code/index.js
+++ code/index.js
@@ -1,7 +1,23 @@
+let btn1 = document.querySelector('.first');
+let btn2 = document.querySelector('.second');
 let box1 = document.querySelector('.box1');
-let box2 = document.querySelector('.box2');
-let btn1 = document.querySelector('.btn1');
-let btn2 = document.querySelector('.btn2');
+let box2 = document.querySelector('.box2')
 
+function getRandomColorId () {
+    let hexCharacters = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f'];
+    let color = '#';
+    for(let i=0; i<6 ; i++) {
+        let randomNumber = Math.floor(Math.random()*16)
+        color = color + hexCharacters[randomNumber];
+    }
+    return color;
+}
+function handleClick(){
+    box1.style.backgroundColor = getRandomColorId()
+}
+function handleMouseMove(){
+    box2.style.backgroundColor = getRandomColorId()
+}
 
-btn1.addEventListener('click' , )
+btn1.addEventListener('click',handleClick);
+btn2.addEventListener('mousemove',handleMouseMove)
